9a2579b42b4792c80878b3d30c23d4393ce709da
Ichii <45918948+IchiiDev@users.noreply.github.com>
2021-01-21 22:53:06 +0100

    Update logo

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: __dirname + "/assets/logo/logo.png",
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true
